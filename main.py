00b0b14f55646f2936d0aeb3f951dde0f99df7fb
Gabryel Gomes Silva Souza <gabryelgomes3030@gmail.com>
2026-05-02 00:12:30 -0300

    Cria crud de locais no mapa com bloqueio de duplicatas

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from routes.search_routes import router as search_router
 from routes.book_routes import router as book_routes
 from routes.review_routes import router as review_router
 from routes.recovery_routes import router as recovery_router
+from routes.location_routes import router as location_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -24,6 +25,7 @@ app.include_router(search_router)
 app.include_router(review_router)
 app.include_router(book_routes)
 app.include_router(recovery_router)
+app.include_router(location_router)
 
 @app.get("/")
 def root():
